f733f1816e5e149d5c27cefae1b1ad04b0e0c63f
niks6996 <111993397+niks6996@users.noreply.github.com>
2023-01-23 10:36:40 +0000

    Create docker-download.sh

---
+++ docker-download.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo yum install -y yum-utils
+
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm -y
+
+sudo yum install docker-ce docker-ce-cli -y
+
+sudo systemctl start docker 
+
+sudo systemctl status docker
+
+sudo systemctl enable docker
